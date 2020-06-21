643c19d918c7dd31ab5d0a87b07eac8e6221d855
Anton Semenov <allcreater@gmail.com>
2020-06-21 03:21:33 +0300

    Added small improvements to uniform buffer

--- src/AT2/OpenGL/GlUniformBuffer.cpp
+++ src/AT2/OpenGL/GlUniformBuffer.cpp
@@ -30,10 +30,9 @@ void SetUniformInternal(GLuint bufferId, GlShaderProgram::UniformBufferInfo* ubi
     if (!ui)
         return;
 
-    //glMapNamedBufferRangeEXT(bufferId, ui->Offset, size, GL_MAP_WRITE_BIT | GL_MAP_INVALIDATE_RANGE_BIT);
-    auto data = reinterpret_cast<GLbyte*>(glMapNamedBufferEXT(bufferId, GL_WRITE_ONLY));
+    auto* const data = reinterpret_cast<GLbyte*>(glMapNamedBufferRangeEXT(bufferId, ui->Offset, sizeof(T), GL_MAP_WRITE_BIT | GL_MAP_INVALIDATE_RANGE_BIT));
     const void* ptr = value_ptr(value);
-    memcpy(data + ui->Offset, ptr, sizeof(T));
+    memcpy(data, ptr, sizeof(T));
     glUnmapNamedBufferEXT(bufferId);
 }
 
@@ -47,10 +46,9 @@ void SetUniformInternal(GLuint bufferId, GlShaderProgram::UniformBufferInfo* ubi
     if (!ui)
         return;
 
-    //glMapNamedBufferRangeEXT(bufferId, ui->Offset, size, GL_MAP_WRITE_BIT | GL_MAP_INVALIDATE_RANGE_BIT);
-    auto data = reinterpret_cast<GLbyte*>(glMapNamedBufferEXT(bufferId, GL_WRITE_ONLY));
+    auto* const data = reinterpret_cast<GLbyte*>(glMapNamedBufferRangeEXT(bufferId, ui->Offset, ui->MatrixStride * C, GL_MAP_WRITE_BIT | GL_MAP_INVALIDATE_RANGE_BIT));
     for (int i = 0; i < C; ++i)
-        memcpy(data + ui->Offset + ui->MatrixStride * i, &(value[i].x), sizeof(MatT::col_type));
+        memcpy(data + ui->MatrixStride * i, &(value[i].x), sizeof(MatT::col_type));
 
     glUnmapNamedBufferEXT(bufferId);
 }
@@ -60,34 +58,7 @@ void GlUniformBuffer::SetUniform(const str &name, const Uniform &value)
 {
     using namespace glm;
 
-    //could be reduced to just one generic lambda, but let it be
-    std::visit(Utils::overloaded{
-        [&](int x) { SetUniformInternal(m_id, m_ubi.get(), name, x); },
-        [&](const ivec2& x) { SetUniformInternal(m_id, m_ubi.get(), name, x); },
-        [&](const ivec3& x) { SetUniformInternal(m_id, m_ubi.get(), name, x); },
-        [&](const ivec4& x) { SetUniformInternal(m_id, m_ubi.get(), name, x); },
-
-        [&](unsigned int x) { SetUniformInternal(m_id, m_ubi.get(), name, x); },
-        [&](const uvec2& x) { SetUniformInternal(m_id, m_ubi.get(), name, x); },
-        [&](const uvec3& x) { SetUniformInternal(m_id, m_ubi.get(), name, x); },
-        [&](const uvec4& x) { SetUniformInternal(m_id, m_ubi.get(), name, x); },
-
-        [&](float x)       { SetUniformInternal(m_id, m_ubi.get(), name, x); },
-        [&](const vec2& x) { SetUniformInternal(m_id, m_ubi.get(), name, x); },
-        [&](const vec3& x) { SetUniformInternal(m_id, m_ubi.get(), name, x); },
-        [&](const vec4& x) { SetUniformInternal(m_id, m_ubi.get(), name, x); },
-        [&](const mat2& x) { SetUniformInternal(m_id, m_ubi.get(), name, x); },
-        [&](const mat3& x) { SetUniformInternal(m_id, m_ubi.get(), name, x); },
-        [&](const mat4& x) { SetUniformInternal(m_id, m_ubi.get(), name, x); },
-
-        [&](double x)       { SetUniformInternal(m_id, m_ubi.get(), name, x); },
-        [&](const dvec2& x) { SetUniformInternal(m_id, m_ubi.get(), name, x); },
-        [&](const dvec3& x) { SetUniformInternal(m_id, m_ubi.get(), name, x); },
-        [&](const dvec4& x) { SetUniformInternal(m_id, m_ubi.get(), name, x); },
-        [&](const dmat2& x) { SetUniformInternal(m_id, m_ubi.get(), name, x); },
-        [&](const dmat3& x) { SetUniformInternal(m_id, m_ubi.get(), name, x); },
-        [&](const dmat4& x) { SetUniformInternal(m_id, m_ubi.get(), name, x); }
-        }, value);
+    std::visit([&](const auto& x) { SetUniformInternal(m_id, m_ubi.get(), name, x); }, value);
 }
 
 void GlUniformBuffer::Bind()
