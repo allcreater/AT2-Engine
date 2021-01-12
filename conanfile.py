4813f383ecab4d55eac2b136c5b380d10903b8ed
Anton Semenov <allcreater@gmail.com>
2021-01-13 00:54:21 +0300

    using of Conan's cmake generator instead of cmake_find_package

--- conanfile.py
+++ conanfile.py
@@ -4,8 +4,8 @@ import shutil, os
 
 class AT2(ConanFile):
    settings = "os", "compiler", "build_type", "arch"
-   requires = "glfw/3.3.2", "glew/2.1.0", "glm/0.9.9.8", "stb/20200203", "assimp/5.0.1", "gtest/1.10.0", "glad/0.1.34", "nlohmann_json/3.9.1"
-   generators = "cmake_find_package"
+   requires = "glfw/3.3.2", "glm/0.9.9.8", "stb/20200203", "assimp/5.0.1", "gtest/1.10.0", "glad/0.1.34", "nlohmann_json/3.9.1"
+   generators = "cmake"
    default_options = { "glad:gl_version": "4.5" }
    #default_options = {"poco:shared": True, "openssl:shared": True} (example for future use)
 
